1b656907375e2af7360cd8e12ac286821f2238a2
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 22:16:43 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,14 +4,9 @@ def nyc_pigeon_organizer(data)
  data.each do |category, hash|
    hash.each do |property, array|
      array.each do |name|
-       if !new_hash.has_key?(name)
-         new_hash[name] = { }
-       end
-      if !new_hash[name].has_key?(category)
-        new_hash[name][category] = { }
-      end
-      if !new_hash[name].include?(property)
-        new_hash[name][category] << property.to_s
+        pigeons[name] ||= {}
+        pigeons[name][property] ||= []
+        pigeons[name][property] << detail.to_s
       end
       end
     end
