e3fe7bad870c7db4797d0cf5e7043a0960eaf02d
Billie Simmons <billiesnyc@gmail.com>
2018-08-22 22:10:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,10 +6,13 @@ def nyc_pigeon_organizer(data)
      array.each do |name|
        if !new_hash.has_key?(name)
          new_hash[name] = { }
+       end
       if !new_hash[name].has_key?(category)
         new_hash[name][category] = { }
+      end
       if !new_hash[name].has_key?(property)
         new_hash[name][category] << property
+      end
       end
     end
   end
